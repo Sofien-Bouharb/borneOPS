a581490ec6d45be8217222fe50a67349407b4223
sofien-bouharb <sofien.bouharb@isimg.tn>
2026-07-25 13:24:34 +0100

    Module 2: add factories for Organization, Site, ChargingStation

---
+++ database/factories/ChargingStationFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Site;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ChargingStationFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'site_id' => Site::factory(),
+            'name' => 'Station ' . fake()->bothify('??-####'),
+            'reference' => fake()->unique()->bothify('REF-????-####'),
+            'serial_number' => fake()->unique()->bothify('SN-????-####'),
+            'model' => fake()->randomElement(['ModelX', 'ModelY', 'ModelZ']),
+            'manufacturer' => fake()->randomElement(['AcmeCharge', 'VoltWorks', 'ChargeTech']),
+            'latitude' => fake()->latitude(),
+            'longitude' => fake()->longitude(),
+            'firmware_version' => 'v' . fake()->numberBetween(1, 5) . '.' . fake()->numberBetween(0, 9),
+            'ocpp_version' => fake()->randomElement(['1.6', '2.0.1']),
+            'ocpp_identifier' => null,
+            'declared_connector_count' => fake()->numberBetween(1, 4),
+            'power_kw' => fake()->randomElement([7.4, 11, 22, 50, 150]),
+        ];
+    }
+}
